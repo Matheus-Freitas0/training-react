c5967415420eb8eb11daf1bbb9bf699070fbe90e
MatheusFreitas <moncaomatheus@gmail.com>
2024-10-29 13:59:34 -0300

    Added react-router-dom and configured routes for Home, Sobre, and Contato pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
 
 function App() {
   return (
-    < Home />
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
