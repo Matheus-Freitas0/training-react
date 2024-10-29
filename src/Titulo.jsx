d0be0c7094cef209b09d0b35840b6cd8a1950d28
MatheusFreitas <moncaomatheus@gmail.com>
2024-10-28 23:53:42 -0300

    Added variables and image to Titulo component

--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -1,7 +1,13 @@
 function Titulo() {
+
+    let nome = 'Matheus Monção'
+    let idade = 18
+    const urlImg = "https://gartic.com.br/imgs/mural/su/supersonic_br/um-desenho-qualquer-5.png"
+
     return (
         <div>
-            <h1>Titulo</h1>
+            <h1>Olá eu sou {nome} e tenho {idade} anos</h1>
+            <img src={urlImg} width={200}/>
             <p>Testando Testando Testando</p>
         </div>
     )
