e4d9807ee3eb8b8ae8fad12a576e516aa7d4ae0e
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-06 14:20:47 -0300

    Updated Alert component to return JSX and added it to MemberListPage

--- src/components/members/Alert.jsx
+++ src/components/members/Alert.jsx
@@ -1,6 +1,11 @@
 
 function Alert() { 
-    console.log("Alert!");
+    return (    
+    <div className="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>The project was registered!</strong> Deu certo lalala.
+    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+    )
 }
 
 export default Alert
--- src/pages/MemberListPage.jsx
+++ src/pages/MemberListPage.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/members/header"
+import Alert from "../components/members/alert"
 
 function MemberListPage() {    
     return (
         <div>
             <Header />
+            <Alert />
         </div>
     )
 }
