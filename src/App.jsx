31834f754e83f3c74d9852b6910808653516de3d
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-06 14:11:59 -0300

    Updated index.html, package-lock.json, package.json, App.jsx, Header.jsx, and main.jsx files; added Bootstrap and Popper.js dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,14 @@ import MemberRegisterPage from './pages/MemberRegisterPage';
 const App = () => (
     <Router>
         <div>
-            <nav>
-                <ul>
-                    <li><Link to="/">Member List</Link></li>
-                    <li><Link to="/register">Register Member</Link></li>
-                </ul>
-            </nav>
             <Routes>
                 <Route path="/" element={<MemberListPage />} />
                 <Route path="/register" element={<MemberRegisterPage />} />
             </Routes>
+                <ul>
+                    <li><Link to="/">Member List</Link></li>
+                    <li><Link to="/register">Register Member</Link></li>
+                </ul>
         </div>
     </Router>
 );
--- src/components/members/Header.jsx
+++ src/components/members/Header.jsx
@@ -1,15 +1,15 @@
-function Header() {
-  return (
-    <>
-      <header>
-        <nav class="navbar">
-          <div class="container-fluid">
-            <p class="nav fst-italic fs-3">Project Manager</p>
-          </div>
-        </nav>
-      </header>
-    </>
-  );
+import './Header.css'
+
+function Header() { 
+    return (
+        <header>
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <p className="nav fst-italic fs-3">Project Manager</p>
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
