540499aa7a2ca8c219178d989319d67ff6afbaf1
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-06 10:15:22 -0300

    Added MemberContainer route and link to menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import Contato from "./pages/Contato";
+import MemberContainer from "./pages/members/MemberContainer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/sobre" element={<Sobre />} />
           <Route path="/contato" element={<Contato />} />
+          <Route path="/members" element={<MemberContainer />} />
         </Routes>
       </BrowserRouter>
     </div>
