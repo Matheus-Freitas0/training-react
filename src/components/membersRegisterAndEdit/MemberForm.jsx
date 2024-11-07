6f645cc7ae0e46b5cf0774f98ef475cba6a9df57
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-07 13:54:58 -0300

    Added API calls for fetching and updating member data, and implemented form submission logic for creating and editing members.

--- src/components/membersRegisterAndEdit/MemberForm.jsx
+++ src/components/membersRegisterAndEdit/MemberForm.jsx
@@ -1,23 +1,70 @@
-import { useParams, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import InputMask from 'react-input-mask';
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import InputMask from "react-input-mask";
 import "./MemberForm.css";
+import axios from "axios";
 
 function MemberForm() {
-
-  const { id } = useParams()
-  const navigate = useNavigate()
+  const { id } = useParams();
+  const navigate = useNavigate();
   const [member, setMember] = useState({
-    name: '',
-    assignment: '',
-    document: '',
-    employee: false
-    })
+    name: "",
+    assignment: "",
+    document: "",
+    employee: false,
+  });
+
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`http://192.168.15.12:8080/members/api/${id}`)
+        .then((response) => {
+          setMember(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching member data:", error);
+        });
+    }
+  }, [id]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (id) {
+
+      axios.put('http://192.168.15.12:8080/members/api', {
+        id: member.id, 
+        employee: member.employee, 
+        assignment: member.assignment 
+      })
+      .then(response => {
+        console.log("Member updated:", response.data);
+        navigate('/'); 
+      })
+      .catch(error => {
+        console.error("Error updating member:", error);
+      });
+    } else {
+      axios.post('http://192.168.15.12:8080/members/api/create', {
+        name: member.name, 
+        employee: member.employee,
+        assignment: member.assignment, 
+        document: member.document, 
+      })
+      .then(response => {
+        console.log("New member created:", response.data);
+        navigate('/'); 
+      })
+      .catch(error => {
+        console.error("Error creating member:", error);
+      });
+    }
+  };
+
 
   return (
     <main className="member-form-container">
       <div className="form-wrapper">
-        <h2 className="form-title">Member Registration</h2>
+        <h2 className="form-title">{id ? "Edit Member" : "Member Registration"}</h2>
 
         <form id="registrationForm" className="registration-form">
           <div className="form-row">
