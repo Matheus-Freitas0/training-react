c3669d0547406363ca2342673d3ea5102ed25fc1
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-07 00:03:23 -0300

    Removed global CSS styles, refactored CSS classes and attributes, and updated JSX syntax in multiple components.

--- src/components/membersRegister/MemberForm.jsx
+++ src/components/membersRegister/MemberForm.jsx
@@ -3,64 +3,64 @@ import "./MemberForm.css";
 function MemberForm(id) {
   return (
     <main>
-      <div class="container my-12">
-        <h2 class="text-center mb-4" id="project-title">
+      <div className="container my-12">
+        <h2 className="text-center mb-4" id="project-title">
           Member Registration
         </h2>
         <form id="registrationForm">
-          <div class="row">
-            <div class="col-md-6 mb-3">
-              <label for="project-name" class="form-label">
+          <div className="row">
+            <div className="col-md-6 mb-3">
+              <label htmlFor="member-name" className="form-label">
                 Member Name
               </label>
               <input
                 type="text"
-                class="form-control"
-                id="Member-name"
+                className="form-control"
+                id="member-name"
                 placeholder="Member name"
                 required
               />
             </div>
-            <div class="col-md-6 mb-3">
-              <label for="project-name" class="form-label">
+            <div className="col-md-6 mb-3">
+              <label htmlFor="assignment" className="form-label">
                 Assignment
               </label>
               <input
                 type="text"
-                class="form-control"
-                id="Assignment"
+                className="form-control"
+                id="assignment"
                 placeholder="Assignment..."
                 required
               />
             </div>
           </div>
 
-          <div class="row">
-            <div class="col-md-6 mb-3">
-              <label for="project-name" class="form-label">
+          <div className="row">
+            <div className="col-md-6 mb-3">
+              <label htmlFor="document" className="form-label">
                 Document
               </label>
               <input
                 type="text"
-                class="form-control"
-                id="Document"
+                className="form-control"
+                id="document"
                 placeholder="Document number..."
                 required
               />
             </div>
 
-            <div class="col-md-6 mb-3">
-              <label for="is-employee" class="form-label">
+            <div className="checkbox col-md-6 mb-3">
+              <label htmlFor="is-employee" className="form-label">
                 Employee
               </label>
               <input
                 type="checkbox"
-                class="form-check-input"
+                className="form-check-input"
                 id="is-employee"
               />
             </div>
           </div>
-          <button type="submit" id="submitButton" class="btn btn-primary">
+          <button type="submit" id="submitButton" className="btn btn-primary">
             Register Member
           </button>
         </form>
@@ -68,4 +68,5 @@ function MemberForm(id) {
     </main>
   );
 }
+
 export default MemberForm;
