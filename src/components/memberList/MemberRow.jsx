5f9f207e00b97b52bd42f8c1778d3e8bd8c7e57a
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-09 22:01:12 -0300

    Updated MemberRow component and MemberTable CSS: reformatted code, added quotes around string literals, and styled edit button

--- src/components/memberList/MemberRow.jsx
+++ src/components/memberList/MemberRow.jsx
@@ -1,21 +1,28 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "./MemberTable.css";
-import React from 'react';
-import useFormattedCPF from '../../services/useFormattedCPF';  
+import React from "react";
+import useFormattedCPF from "../../services/useFormattedCPF";
 
 function MemberRow({ member }) {
   const navigate = useNavigate();
-  const handleEditClick = () => {navigate(`/register/${member.id}`);};
-  const formattedCPF = useFormattedCPF(member.document);  
+  const handleEditClick = () => {
+    navigate(`/register/${member.id}`);
+  };
+  const formattedCPF = useFormattedCPF(member.document);
 
   return (
     <tr>
       <td>{member.name}</td>
-      <td>{member.employee ? 'Yes' : 'No'}</td>
+      <td>{member.employee ? "Yes" : "No"}</td>
       <td>{member.assignment}</td>
       <td className="d-none d-sm-table-cell">{formattedCPF}</td>
-      <td className="btns">
-        <button className="btn btn-primary" onClick={handleEditClick}>Edit</button>
+      <td className="text-center align-middle">
+        <button
+          className="btn btn-primary edit-btn"
+          onClick={handleEditClick}
+        >
+          Edit
+        </button>
       </td>
     </tr>
   );
